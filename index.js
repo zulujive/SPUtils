fc8e63f83df1b143b9eebad8515d98c4ede76f2b
zulujive <51298470+zulujive@users.noreply.github.com>
2023-04-11 01:01:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -16,51 +16,7 @@ app.get('/', (req, res) => {
     access_type: 'offline',
     scope: SCOPES,
   });
-  const html = `
-    <html>
-        <head>
-            <title>STEMPlace Site Utils</title>
-            <style>
-                h1 {
-                    color: #fff;
-		    text-align: center;
-                }
-                .container {
-                    margin: 0 auto;
-                    padding: 50px;
-                    background: linear-gradient(to bottom right, #F472B6, #BD10E0, #FFD700);
-                    color: #fff;
-                    border-radius: 10px;
-                    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-                    font-family: Arial, Helvetica, sans-serif;
-                }
-                @media screen and (max-width: 768px) {
-                    .container {
-                        padding: 30px;
-                    }
-                }
-            </style>
-        </head>
-        <body>
-            <div class="container">
-              <h1>STEMPlace OAuth Verification Tool</h1>
-              <p>This tool is useful for retrieving data for verifying
-              your identity on STEMPlace. <b>DO NOT</b> share the information
-              provided by this website, it may be sensitive!<br><br><br>
-	      This tool is intended for people who:
-	      <ul>
-	        <li>Initially signed up with their personal email (not @(s.)stemk12.org)</li>
-		<li>Are soon going to graduate (congrats!)</li>
-		<li>Former members of the STEM community</li>
-		<li>Anyone who was active and ended in good standing on STEMPlace v1 and v2</li>
-	      </ul>
-              </p>
-              <a href="${authUrl}">Authorize</a>
-            </div>
-        </body>
-    </html>
-  `;
-  res.send(html);
+  res.send(express.static(__dirname));
 });
 
 app.get('/oauth2callback', (req, res) => {
